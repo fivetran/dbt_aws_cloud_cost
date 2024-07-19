2e6eb4b92d125990f39af81a7e32bd28117ce902
Jamie Rodriguez <65564846+fivetran-jamie@users.noreply.github.com>
2024-07-19 14:22:36 -0700

    service draft

--- models/aws_cloud_cost__overview.sql
+++ models/aws_cloud_cost__overview.sql
@@ -19,29 +19,29 @@ to their IDs so we can fill these in when needed. #}
 usage_account_mapping as (
 
     select 
-        line_item_usage_account_id,
-        line_item_usage_account_name,
+        usage_account_id,
+        usage_account_name,
         source_relation,
-        max(line_item_usage_start_date) as latest_start_date
+        max(usage_start_date) as latest_start_date
 
     from source_report
-    where line_item_usage_account_name is not null
+    where usage_account_name is not null
     group by 1,2,3
 ),
 
 usage_account_names as (
 
     select
-        sub.line_item_usage_account_id,
-        sub.line_item_usage_account_name,
+        sub.usage_account_id,
+        sub.usage_account_name,
         sub.source_relation
     from (
         {# In case the account name as been updated, let's ensure we're only grabbing the most recent one #}
         select 
-            line_item_usage_account_id,
-            line_item_usage_account_name,
+            usage_account_id,
+            usage_account_name,
             source_relation,
-            row_number() over (partition by line_item_usage_account_id, source_relation order by latest_start_date desc) = 1 as is_latest_name
+            row_number() over (partition by usage_account_id, source_relation order by latest_start_date desc) = 1 as is_latest_name
         from usage_account_mapping
     ) as sub where is_latest_name
 ),
@@ -85,14 +85,14 @@ fields as (
         report, 
 
         {# Period Details #}
-        cast({{ dbt.date_trunc('day', 'line_item_usage_start_date') }} as date) as usage_start_date,
-        cast({{ dbt.date_trunc('day', 'line_item_usage_end_date') }} as date) as usage_end_date, -- keep just in case
+        cast({{ dbt.date_trunc('day', 'usage_start_date') }} as date) as usage_start_date,
+        cast({{ dbt.date_trunc('day', 'usage_end_date') }} as date) as usage_end_date, -- keep just in case
         billing_period_start_date,
         billing_period_end_date,
 
         {# Account Details #}
-        source_report.line_item_usage_account_id,
-        coalesce(source_report.line_item_usage_account_name, usage_account_names.line_item_usage_account_name) as line_item_usage_account_name,
+        source_report.usage_account_id,
+        coalesce(source_report.usage_account_name, usage_account_names.usage_account_name) as usage_account_name,
         source_report.bill_payer_account_id,
         coalesce(source_report.bill_payer_account_name, billing_account_names.bill_payer_account_name) as bill_payer_account_name,
 
@@ -105,67 +105,70 @@ fields as (
         line_item_tax_type,
 
         {# Pricing Details #}
-        pricing_purchase_option,
+        purchase_option,
         pricing_term,
         product_fee_code,
         product_fee_description,
 
         {# Units #}
         pricing_unit,
-        line_item_usage_type,
-        line_item_currency_code,
+        usage_type,
+        currency_code,
         
         {# Line Item Service Details #}
         line_item_description,
-        line_item_resource_id, -- null unless you've enabled `INCLUDE RESOURCES` in your AWS CUR configuration
-        line_item_product_code,
-        product_service_code,
+        resource_id, -- null unless you've enabled `INCLUDE RESOURCES` in your AWS CUR configuration
+        product_code,
         product_name,
+        product_service_code, -- service is within product
         product_family,
         operation,
 
+        {# Product Details - Compute #}
+        instance_type,
+        instance_family,
+
         {# Product Details - s3 #}
-        product_location,
-        product_location_type,
-        product_region_code,
-        line_item_availability_zone,
+        location,
+        location_type,
+        region_code,
+        availability_zone,
 
         {# Product Details - Data transfers/movement #}
-        product_from_location,
-        product_from_location_type,
-        product_from_region_code,
-        product_to_location,
-        product_to_location_type,
-        product_to_region_code,
-
-        {# Product Details - Compute #}
-        product_instance_family,
-        product_instance_type
+        from_location,
+        from_location_type,
+        from_region_code,
+        to_location,
+        to_location_type,
+        to_region_code
 
         {{ fivetran_utils.persist_pass_through_columns(pass_through_variable='aws_cloud_cost_report_pass_through_columns') }},
 
         {# Usage Metrics #}
-        sum(coalesce(line_item_usage_amount, 0)) as usage_amount,
-        sum(coalesce(line_item_normalized_usage_amount, 0)) as normalized_usage_amount,
-        max(line_item_normalization_factor) as line_item_normalization_factor,
+        sum(coalesce(usage_amount, 0)) as usage_amount,
+        sum(coalesce(normalized_usage_amount, 0)) as normalized_usage_amount,
+        max(normalization_factor) as normalization_factor,
 
         {# Cost Metrics - General #}
-        sum(coalesce(line_item_blended_cost, 0)) as blended_cost,
-        sum(coalesce(line_item_unblended_cost, 0)) as unblended_cost,
-        sum(coalesce(pricing_public_on_demand_cost, 0)) as pricing_public_on_demand_cost,
-        avg(line_item_blended_rate) as avg_blended_rate,
-        max(line_item_unblended_rate) as avg_unblended_rate,
-        avg(pricing_public_on_demand_rate) as avg_pricing_public_on_demand_rate,
+        sum(coalesce(blended_cost, 0)) as blended_cost,
+        sum(coalesce(unblended_cost, 0)) as unblended_cost,
+        sum(coalesce(public_on_demand_cost, 0)) as public_on_demand_cost,
+        avg(blended_rate) as avg_blended_rate,
+        avg(unblended_rate) as avg_unblended_rate,
+        avg(public_on_demand_rate) as avg_public_on_demand_rate,
+        count(*) as count_line_items,
 
-        {# Cost & Usage Metrics - Reservations #}
+        {# Cost & Usage Metrics - Reservations 
+            Using MAX's under the assumption that there is a 1:Many relationship between Reservations and Line Items 
+        #}
         max(reservation_amortized_upfront_cost_for_usage) as reservation_amortized_upfront_cost_for_usage,
         max(reservation_amortized_upfront_fee_for_billing_period) as reservation_amortized_upfront_fee_for_billing_period,
         max(reservation_effective_cost) as reservation_effective_cost,
-        max(reservation_number_of_reservations) as reservation_number_of_reservations,
-        max(reservation_normalized_units_per_reservation) as reservation_normalized_units_per_reservation,
-        max(reservation_units_per_reservation) as reservation_units_per_reservation,
-        max(reservation_total_reserved_normalized_units) as reservation_total_reserved_normalized_units,
-        max(reservation_total_reserved_units) as reservation_total_reserved_units,
+        max(number_of_reservations) as number_of_reservations,
+        max(normalized_units_per_reservation) as normalized_units_per_reservation,
+        max(units_per_reservation) as units_per_reservation,
+        max(total_reserved_normalized_units) as total_reserved_normalized_units,
+        max(total_reserved_units) as total_reserved_units,
         max(reservation_recurring_fee_for_usage) as reservation_recurring_fee_for_usage,
         max(reservation_unused_amortized_upfront_fee_for_billing_period) as reservation_unused_amortized_upfront_fee_for_billing_period,
         max(reservation_unused_normalized_unit_quantity) as reservation_unused_normalized_unit_quantity,
@@ -186,7 +189,7 @@ fields as (
         on source_report.bill_payer_account_id = billing_account_names.bill_payer_account_id
         and source_report.source_relation = billing_account_names.source_relation
     left join usage_account_names
-        on source_report.line_item_usage_account_id = usage_account_names.line_item_usage_account_id
+        on source_report.usage_account_id = usage_account_names.usage_account_id
         and source_report.source_relation = usage_account_names.source_relation
 
     {{ dbt_utils.group_by(n=42 + var('aws_cloud_cost_report_pass_through_columns',[])|length) }}
@@ -200,8 +203,8 @@ final as (
         'usage_end_date',
         'billing_period_start_date',
         'billing_period_end_date',
-        'line_item_usage_account_id',
-        'line_item_usage_account_name',
+        'usage_account_id',
+        'usage_account_name',
         'bill_payer_account_id',
         'bill_payer_account_name',
         'bill_invoice_id',
@@ -210,32 +213,32 @@ final as (
         'bill_type', 
         'line_item_type',
         'line_item_tax_type',
-        'pricing_purchase_option',
+        'purchase_option',
         'pricing_term',
         'product_fee_code',
         'product_fee_description',
         'pricing_unit',
-        'line_item_usage_type',
-        'line_item_currency_code',
+        'usage_type',
+        'currency_code',
         'line_item_description',
-        'line_item_resource_id',
-        'line_item_product_code',
+        'resource_id',
+        'product_code',
         'product_service_code',
         'product_name',
         'product_family',
         'operation',
-        'product_location',
-        'product_location_type',
-        'product_region_code',
-        'line_item_availability_zone',
-        'product_from_location',
-        'product_from_location_type',
-        'product_from_region_code',
-        'product_to_location',
-        'product_to_location_type',
-        'product_to_region_code',
-        'product_instance_family',
-        'product_instance_type'
+        'instance_family',
+        'instance_type',
+        'location',
+        'location_type',
+        'region_code',
+        'availability_zone',
+        'from_location',
+        'from_location_type',
+        'from_region_code',
+        'to_location',
+        'to_location_type',
+        'to_region_code'
     ] 
 -%}
 
